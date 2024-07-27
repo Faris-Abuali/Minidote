41f708d867f2b70efabdbca31a037c3a741f6342
Mahmoud Hamido <nif04fep@rptu.de>
2024-07-27 03:39:49 +0200

    cleaned up test code

--- ex_final/minidote/test/persistent_log_test.exs
+++ ex_final/minidote/test/persistent_log_test.exs
@@ -5,18 +5,13 @@ defmodule PersistentLogTest do
     dc1 = TestSetup.start_node(:minidote_log_test)
     key = {"upvotes", :antidote_crdt_counter_pn, "minireddit"}
 
-    # test/persistent_log_test.exs:4
-    # ** (MatchError) no match of right hand side value: {:ok, [{{"upvotes", :antidote_crdt_counter_pn, "minireddit"}, -5}], %{}}
     {:ok, vc} =
       :rpc.call(dc1, :"Elixir.Minidote", :update_objects, [
         [{key, :increment, 20}],
         :ignore
       ])
 
-    # Force kill the minidote server process
     :rpc.call(dc1, :"Elixir.Minidote", :unsafe_force_crash, [])
-    # NOTE: Does the log crash with the server? if not then maybe we should stop it.
-    # Either seperate the log from the server or close the log with the server (when it crashes).
     {:ok, [{^key, 20}], _} =
       :rpc.call(dc1, :"Elixir.Minidote", :read_objects, [
         [key],
@@ -24,32 +19,6 @@ defmodule PersistentLogTest do
       ])
 
     TestSetup.stop_node(dc1)
-    # # Spawn a process
-    # # TODO: clear log before test
-    # name = "persistent_unit_test"
-    # File.rm(PersistentLog.get_log_path(name))
-    # {:ok, log_proc} = PersistentLog.start_link(self(), name)
-
-    # # log an update
-    # vc = Vectorclock.new()
-    # key = {"location", :antidote_crdt_counter_fat, "mensa"}
-
-    # # TODO: fix this test, we broke how :persist works.
-    # :ok =
-    #   GenServer.call(
-    #     log_proc,
-    #     {:persist,
-    #       vc,
-    #      [
-    #        {vc, key, :increment, 42},
-    #        {vc, key, :decrement, 42}
-    #      ]}
-    #   )
-
-    # # read the log
-    # read_result = GenServer.call(log_proc, :get_entries)
-    # # pattern match
-    # {:ok, [[{^vc, key, :increment, 42}, {^vc, key, :decrement, 42}]]} = read_result
   end
 
   test "server should recover state after crashing" do
